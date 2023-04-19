1471f4c61687849590d3e64bd5a370e9779d6d90
Alex <alexey.p_96@mail.ru>
2023-04-19 17:38:29 +0300

    Update views.py

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import generics
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
